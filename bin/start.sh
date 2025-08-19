778e565555531b5a8c412a97b276d7f292b60750
personjerry <personjerry@gmail.com>
2025-08-18 23:26:21 -0400

    Initial import

--- bin/start.sh
+++ bin/start.sh
@@ -6,4 +6,9 @@ echo "Starting the application..."
 echo "Environment: ${ENV:-development}"
 
 # Add your application startup commands here
+
+
+source .venv/bin/activate
+pip install -r requirements.txt
+
 echo "Application started successfully!" 
---
+++ messaging_service/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from messaging_service.users.models import User
+from messaging_service.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
